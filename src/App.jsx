11721972238e316b98b5cc7bf03f35eea4ae4af6
Anmol Yadav <anmolyadav03unnao@gmail.com>
2024-01-18 19:03:38 +0530

    fixed/enter-key-use

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,7 @@ function App() {
                 <button
                   className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md px-2 py-0.5 bg-green-600 text-white"
                   onClick={swap}
+                  type="button"
                 >
                   Swap
                 </button>
